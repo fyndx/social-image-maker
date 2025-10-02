e83694a83f2293b0157d62051384ac8ce4ac8e0a
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2025-10-02 22:35:58 +0530

    Refactor Prisma models to include default UUIDs for IDs and implement… (#2)

---
+++ apps/server/src/routers/projects.ts
@@ -0,0 +1,63 @@
+import { z } from "zod";
+import prisma from "@/db";
+import { protectedProcedure } from "@/lib/orpc";
+
+export const projectsRouter = {
+  getAll: protectedProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).optional(),
+        offset: z.number().min(0).optional(),
+      })
+    )
+    .handler(async ({ context, input }) => {
+      const limit = input?.limit ?? 20; // Default limit to 20
+      const offset = input?.offset ?? 0; // Default offset to 0
+      const projects = await prisma.project.findMany({
+        where: {
+          userId: context.session.user.id,
+        },
+        take: limit,
+        skip: offset,
+        orderBy: { createdAt: "desc" },
+      });
+      return projects;
+    }),
+
+  create: protectedProcedure
+    .input(z.object({ name: z.string(), domain: z.url() }))
+    .handler(async ({ context, input }) => {
+      const project = await prisma.project.create({
+        data: {
+          userId: context.session.user.id,
+          name: input.name,
+          domain: input.domain,
+        },
+      });
+      return project;
+    }),
+
+  getById: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .handler(async ({ context, input }) => {
+      const project = await prisma.project.findUnique({
+        where: {
+          id: input.id,
+          userId: context.session.user.id,
+        },
+      });
+      return project;
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .handler(async ({ context, input }) => {
+      const project = await prisma.project.delete({
+        where: {
+          id: input.id,
+          userId: context.session.user.id,
+        },
+      });
+      return project;
+    }),
+};
