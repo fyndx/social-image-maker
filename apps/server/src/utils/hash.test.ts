ab4668137c5fb0a456aa35dbba44fa194b104784
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2025-10-29 23:17:56 +0530

    use snapshot for hash test

--- apps/server/src/utils/hash.test.ts
+++ apps/server/src/utils/hash.test.ts
@@ -4,6 +4,6 @@ import { getSha256Hash } from './hash'
 describe('getSha256Hash', () => {
   test('hash string', () => {
     const url = 'https://fyndx.io'
-    expect(getSha256Hash(url)).toBe('373c78fbd0749c68178c505ee70d3094d05acf3e12562830cccc7c46b3f09a06')
+    expect(getSha256Hash(url)).toMatchSnapshot()
   })
 })
