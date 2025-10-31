af87af3b4ffc839d9a2a7d7927689434e8fd30d2
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2025-10-31 12:53:37 +0530

    Remove unused boilerplate code

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -1,7 +1,6 @@
 import type { RouterClient } from "@orpc/server";
 import { protectedProcedure, publicProcedure } from "../lib/orpc";
 import { projectsRouter } from "./projects";
-import { todoRouter } from "./todo";
 
 export const appRouter = {
   healthCheck: publicProcedure.handler(() => "OK"),
@@ -10,7 +9,6 @@ export const appRouter = {
     user: context.session?.user,
   })),
   projects: projectsRouter,
-  todo: todoRouter,
 };
 export type AppRouter = typeof appRouter;
 export type AppRouterClient = RouterClient<typeof appRouter>;
